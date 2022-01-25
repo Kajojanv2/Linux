292a59aa826f5cd905a231386c691028f76e3575
Kajojan <kajetan.jankowski@interia.pl>
2022-01-25 18:21:15 +0100

    zad

--- test/test_main.py
+++ test/test_main.py
@@ -7,7 +7,7 @@ def test_add():
     assert main.add(3.9, 4) == 7
     assert main.add(3.9, 4.1) == 7
 
-def test_to_sentence()
+def test_to_sentence():
     assert main.to_sentence('apple') == 'Apple'
     assert main.to_sentence('Apple trees') == 'Apple trees.'
     assert main.to_sentence('Apple trees.') == 'Apple trees.'
