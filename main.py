370087cfb9874afcb89060f7ee609e70ead69df0
Kajojan <kajetan.jankowski@interia.pl>
2022-01-25 20:08:44 +0100

    zad

--- main.py
+++ main.py
@@ -4,9 +4,3 @@ import math
 def add(a,b) -> int:
      return math.floor(a+b)
 
-def to_sentence(s) -> str:
-    s=s.capitalize()
-    if s.endswith('.'):
-        return str
-    else:
-        return s + '.'
