cc701463fd2a38c6b1169431b1ae96d4a3f80431
Kajojan <kajetan.jankowski@interia.pl>
2022-01-25 18:19:27 +0100

    zad

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-def funkcja1():
-    i = 1
-    while i < 6:
-        print(i)
-        i += 1
+
+import os
+import math
+def add(a,b) -> int:
+     return math.floor(a+b)
+
+def to_sentence(s) -> str:
+    s=s.capitalize()
+    if s.endswith('.'):
+        return str
     else:
-        print("i is no longer less than 6")
-        
-        
-def silnia():
-    n = 5
-    silnia = 1
-    while n > 0:
-        silnia = silnia * n
-        n = n - 1
-    return (silnia)
+        return s + '.'
--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,18 @@
 import main
 
 
-def test_silnia():
-    assert main.silnia()==120
+def test_add():
+    assert main.add(3, 4) == 7
+    assert main.add(3.5, 4) == 7
+    assert main.add(3.9, 4) == 7
+    assert main.add(3.9, 4.1) == 7
+
+def test_to_sentence()
+    assert main.to_sentence('apple') == 'Apple'
+    assert main.to_sentence('Apple trees') == 'Apple trees.'
+    assert main.to_sentence('Apple trees.') == 'Apple trees.'
+
+
+
+
+
